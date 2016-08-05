5fa4f9f04e839523b3bbedf0b186da786713ec63
Joerg <joerg.patzer@me.com>
2016-08-05 02:57:01 +0200

    create first draft of docker-bot module

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 let router = express.Router();
 import dockerClient from '../lib/docker-client';
+import dockerBot from '../lib/docker-bot';
 
 // check whether we can get access to the docker remote API
 dockerClient.then(docker => {
@@ -21,6 +22,14 @@ router.get('/', (req, res, next) => {
 
 router.get('/bot/start', (req, res, next) => {
 
+    /*dockerBot(dockerClient).then(result => {
+     console.log("Promise Success");
+     res.send("Bot started");
+     })
+     .catch(err => {
+     console.error("Promise Failure:", err);
+     process.exit(1);
+     });*/
     res.send("Bot is sleeping");
 
 });
